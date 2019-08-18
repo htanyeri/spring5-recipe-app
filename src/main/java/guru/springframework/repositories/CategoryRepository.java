3b4f72d24cfb9211b156b4f9a1cb5a78f1f0f02e
htanyeri <haci.tanyeri@gmail.com>
2019-08-18 02:27:39 -0400

    Added JPA query methods

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,8 +3,11 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by htanyeri on 8/18/2019.
  */
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -3,8 +3,11 @@ package guru.springframework.repositories;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by htanyeri on 8/18/2019.
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
