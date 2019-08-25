b49890b41d2f9eadd49ca50af46f8c51fa383e15
htanyeri <haci.tanyeri@gmail.com>
2019-08-25 15:37:39 -0400

    adding update functionality

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.converters.RecipeCommandToRecipe;
 import guru.springframework.converters.RecipeToRecipeCommand;
 import guru.springframework.domain.Recipe;
@@ -51,6 +52,27 @@ public class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipeCommandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull("Null recipe returned", commandById);;
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository,never()).findAll();
+    }
+
+    @Test
     public void getRecipesTest() {
         Recipe recipe = new Recipe();
         HashSet recipeData = new HashSet();
@@ -60,6 +82,7 @@ public class RecipeServiceImplTest {
         Set<Recipe> recipes = recipeService.getRecipes();
         assertEquals(1,recipes.size());
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository,never()).findById(anyLong());
     }
 
 }
