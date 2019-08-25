3dfa9070c34f1d7e17a2a47467507f430bf6f61c
htanyeri <haci.tanyeri@gmail.com>
2019-08-25 12:25:48 -0400

    updates

---
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Created by htanyeri on 8/23/2019.
+ */
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void testRecipeService() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+}
